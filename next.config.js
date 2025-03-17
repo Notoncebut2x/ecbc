02de6d8062217e9af7dd67cbd228f58166e2d693
notoncebut2x <4581716+Notoncebut2x@users.noreply.github.com>
2025-03-17 15:27:50 -0400

    Update Next.js config to disable caching

--- next.config.js
+++ next.config.js
@@ -5,6 +5,21 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  generateEtags: false,
+  poweredByHeader: false,
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig 
